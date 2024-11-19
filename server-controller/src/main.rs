4bdeec15a22deb684309aff06f221fb261d9913f
Daka-0424 <userjh41001@gmail.com>
2024-11-19 13:24:52 +0900

    feat: axum

--- server-controller/src/main.rs
+++ server-controller/src/main.rs
@@ -1,3 +1,19 @@
-fn main(){
-  println!("Hello, world! Controller");
+use axum::{Router, routing::get, response::Html};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, world!</h1>")
 }
